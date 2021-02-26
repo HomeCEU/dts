b7f81e6b5f0b7e3210b4bce682fe08f04084bec2
dan <danmcadams@gmail.com>
2021-02-26 17:40:00 -0600

    methods now takes a comma separated string and converts it to an array when built

--- api/Route.php
+++ api/Route.php
@@ -2,12 +2,12 @@
 namespace HomeCEU\DTS\Api;
 
 class Route {
-  public $method;
+  public $methods;
   public $uri;
   public $function;
 
-  public function __construct($method, $uri, $function) {
-    $this->method = $method;
+  public function __construct(string $methods, string $uri, string $function) {
+    $this->methods = explode(',', str_replace(' ', '', $methods));
     $this->uri = $uri;
     $this->function = $function;
   }
