ea938ba5af3bfc84d9d4015fd482970d4967a16e
dan <danmcadams@gmail.com>
2021-03-18 21:24:56 -0500

    get rid of keys() method, update properties with type hints

--- src/DTS/UseCase/AddTemplateRequest.php
+++ src/DTS/UseCase/AddTemplateRequest.php
@@ -7,19 +7,10 @@ namespace HomeCEU\DTS\UseCase;
 use HomeCEU\DTS\AbstractEntity;
 
 class AddTemplateRequest extends AbstractEntity {
-  public $docType;
-  public $templateKey;
-  public $author;
-  public $body;
-
-  protected static function keys(): array {
-    return [
-        'docType',
-        'templateKey',
-        'author',
-        'body',
-    ];
-  }
+  public string $docType;
+  public string $templateKey;
+  public string $author;
+  public string $body;
 
   public static function fromState(array $state): AbstractEntity {
     return parent::fromState($state)->validate();
