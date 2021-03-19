26a19454888d62ad068b6c477ceaf0eced516c76
dan <danmcadams@gmail.com>
2021-03-19 16:02:11 -0500

    add Required Keys message if $keys array is not empty

--- src/DTS/UseCase/Exception/InvalidRequestException.php
+++ src/DTS/UseCase/Exception/InvalidRequestException.php
@@ -7,6 +7,8 @@ namespace HomeCEU\DTS\UseCase\Exception;
 class InvalidRequestException extends \Exception {
   public function __construct($message = "", array $keys = [], $code = 0) {
     parent::__construct($message, $code);
-    $this->message = $message . implode(', ', $keys);
+    $this->message = empty($keys)
+        ? $message
+        : $message . "Required Keys: " . implode(', ', $keys);
   }
 }
