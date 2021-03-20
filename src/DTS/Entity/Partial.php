9e9148804a853dd6ee5b2212bb569faa6ddd4c49
dan <danmcadams@gmail.com>
2021-03-20 15:36:57 -0500

    CEMS-2301 implement Partial repository and persistence.

---
+++ src/DTS/Entity/Partial.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\DTS\Entity;
+
+
+use HomeCEU\DTS\AbstractEntity;
+
+class Partial extends AbstractEntity {
+  public string $id;
+  public string $name;
+  public string $docType;
+  public string $body;
+  public string $author;
+  public \DateTime $createdAt;
+}
---
+++ src/DTS/Persistence/PartialPersistence.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\DTS\Persistence;
+
+
+use HomeCEU\DTS\Db\Connection;
+
+class PartialPersistence extends AbstractPersistence {
+  const TABLE = 'partials';
+  const ID_COL = 'partial_id';
+
+  private array $map = [
+      'id' => 'partial_id',
+      'docType' => 'doc_type',
+      'name' => 'name',
+      'createdAt' => 'created_at'
+  ];
+
+  public function __construct(Connection $db) {
+    parent::__construct($db);
+    $this->useKeyMap($this->map);
+  }
+}
