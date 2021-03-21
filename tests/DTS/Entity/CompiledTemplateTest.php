75b2934971d943e17bd5ee05f626c43e8bc51232
Dan McAdams <danmcadams@gmail.com>
2021-03-21 16:16:12 -0500

    remove need for faker, was used for creating a new datetime object.

--- tests/DTS/Entity/CompiledTemplateTest.php
+++ tests/DTS/Entity/CompiledTemplateTest.php
@@ -6,24 +6,15 @@ namespace DTS\Entity;
 
 use HomeCEU\DTS\Entity\CompiledTemplate;
 use HomeCEU\Tests\DTS\TestCase;
-use HomeCEU\Tests\Faker;
 use PHPUnit\Framework\Assert;
 use Ramsey\Uuid\Uuid;
 
 class CompiledTemplateTest extends TestCase {
-  public $iso8601;
-
-  protected function setUp(): void {
-    parent::setUp();
-    $fake = Faker::generator();
-    $this->iso8601 = $fake->iso8601;
-  }
-
   public function testBuildFromState(): void {
       $state = [
           'templateId' => Uuid::uuid1()->toString(),
           'body' => '<?php /* compiled template */ ?>',
-          'createdAt' => new \DateTime($this->iso8601)
+          'createdAt' => new \DateTime()
       ];
       $ct = CompiledTemplate::fromState($state);
       Assert::assertEquals($state, $ct->toArray());
