568d3df1d2352eefd3f0910ebbf079f8fbce1227
Dan <danmcadams@gmail.com>
2021-03-22 17:18:30 -0500

    Di Container now loads in service files based on environment

--- api/DiContainer.php
+++ api/DiContainer.php
@@ -4,17 +4,20 @@
 namespace HomeCEU\DTS\Api;
 
 
-use HomeCEU\DTS\Db;
-use HomeCEU\DTS\Render\TemplateCompiler;
-use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Slim\Container;
 
 class DiContainer extends Container implements ContainerInterface {
   public function __construct(array $values = []) {
-    if ($values == []) {
-      $values = include __DIR__."/services.php";
+    $values = array_merge($values, include __DIR__."/services.php");
+    parent::__construct($this->loadEnvironmentalServices($values));
+  }
+
+  protected function loadEnvironmentalServices(array $values): array {
+    $env = strtolower(getenv('APP_ENV'));
+    if (is_file(__DIR__ . "/services.{$env}.php")) {
+      $values = array_merge($values, include __DIR__ . "/services.{$env}.php");
     }
-    parent::__construct($values);
+    return $values;
   }
 }
