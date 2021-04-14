a191290e6d65180ad30dacd48a958017a9801193
dan <danmcadams@gmail.com>
2021-04-14 14:11:45 -0500

    CEMS-2340 add comment explaining regex

--- src/DTS/Render/RenderHelper.php
+++ src/DTS/Render/RenderHelper.php
@@ -6,6 +6,7 @@ namespace HomeCEU\DTS\Render;
 
 class RenderHelper {
   public static function extractPartials(string $template): array  {
+    // finds strings matching {{> any-text-or-whitespace }}
     preg_match_all('/{{>([^\}}]+)}}/', $template, $matches);
     return !empty($matches[1]) ? array_map('trim', $matches[1]) : [];
   }
